1a9eb88bc28ddaba8ddf01431cb61c789f2e7a28
Aaron Brooks <brooksa321@gmail.com>
2018-06-03 14:02:09 -0400

    Changed prime factor project

--- LargestPrimeFactor/main.go
+++ LargestPrimeFactor/main.go
@@ -2,37 +2,37 @@ package main
 
 import (
 	"fmt"
-	"math/big"
+	"math"
 )
 
-// The prime factors of 13195 are 5, 7, 13 and 29.
-// What is the largest prime factor of the number 600851475143 ?
-var primeNums []int64
-var primeFactors []int64
-var count int64
+func main() {
+	var a int64 = 600851475143
 
-func prime(num int64) []int64 {
-	i := big.NewInt(num)
-	if i.ProbablyPrime(1) == true {
-		primeNums = append(primeNums, num)
-	}
-	return primeNums
-}
-
-func areFactors(primes []int64) []int64 {
-	for _, i := range primes {
-		if 600851475143%i == 0 {
-			primeFactors = append(primeFactors, i)
+	for b := sqrt(a); b >= 1; b-- {
+		if a%b == 0 && isPrime(b) {
+			fmt.Println(b)
+			break
 		}
 	}
-	return primeFactors
 }
-func main() {
-	for count = 0; count <= 600851475143; count++ {
-		prime(count)
+
+func isPrime(a int64) bool {
+	if a <= 1 {
+		return false
 	}
 
-	areFactors(primeNums)
-	fmt.Println(primeFactors[len(primeFactors)-1])
+	for b := sqrt(a); b >= 1; b-- {
+		if b == 1 {
+			return true
+		}
+		if a%b == 0 {
+			return false
+		}
+	}
 
+	return true
+}
+
+func sqrt(a int64) int64 {
+	return int64(math.Sqrt(float64(a)))
 }
